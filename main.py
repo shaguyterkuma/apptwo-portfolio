6c5ec39283842831625d11bd830f0946ccc39234
jayden <shaguyterkuma@gmail.com>
2025-06-01 18:04:55 -0500

    inital commit

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-import streamlit as st
+from tkinter import Image
 
+import row
+import streamlit as st
+import pandas
+from PIL import Image
+from PIL.ImageOps import expand
 
 col1,col2 = st.columns(2)
 col3 = st.columns(1)
 
 with col1:
-    st.image("images/photo.png")
+    img = Image.open("images/IMG_7589.jpg")
+    rot_img = img.rotate(-90,expand=True)
+    st.image(rot_img)
 
 with col2:
     st.title("Terkuma Sesugh Shaguy")
@@ -18,3 +25,22 @@ with col2:
 
 morestuff = """Below you can find some of the apps I have built in python , Feel free to contact me """
 st.write(morestuff)
+
+col4,empty_col,col5 = st.columns([1.5,0.5,1.5]) #The numbers represent the ratio dimensions of the columns The first colum would be three times wider than the middle column adn the first and the third column are the same width
+
+df = pandas.read_csv("data.csv",sep=";")
+
+with col4:
+    for index,row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source code]({row['url']})")
+
+with col5:
+    for index,row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image( "images/" + row["image"])
+        st.write(f"[Source code]({row['url']})")
+
